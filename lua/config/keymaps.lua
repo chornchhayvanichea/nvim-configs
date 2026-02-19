f15b7ebc5e55256246e594f250d3084640bef63d
Chea <chornchhayvanichea@gmail.com>
2026-02-19 23:48:09 +0700

    .

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,9 @@ vim.g.mapleader = " "
 
 --vim.keymap.set("n", "<leader>o", ":update<CR> :source<CR>")
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
@@ -11,7 +13,7 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 vim.keymap.set("n", "<leader><S-t>", ":tabnew<CR>")
 
 -- buffers navigations
-vim.keymap.set("n", "<leader><leader>", "<C-^>")
+--vim.keymap.set("n", "<leader><leader>", "<C-^>")
 --vim.keymap.set("n", "<leader>n", ":bnext<CR>")
 --vim.keymap.set("n", "<leader>p", ":bprev<CR>")
 
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,12 +10,39 @@ return {
         ["<C-h>"] = { disabled = true },
         ["<C-v>"] = "actions.select_vsplit", -- Change to C-v for vertical split
         ["<C-s>"] = "actions.select_split", -- Keep C-s for horizontal split
+        ["<leader>w"] = { "actions.parent", mode = "n" },
       },
+      --      -- Configuration for the floating window in oil.open_float
+      --      float = {
+      --        -- Padding around the floating window
+      --        padding = 4,
+      --        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+      --        max_width = 0.7,
+      --        max_height = 0.5,
+      --        border = nil,
+      --        win_options = {
+      --          winblend = 0,
+      --        },
+      --        -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+      --        get_win_title = nil,
+      --        -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+      --        preview_split = "auto",
+      --        -- This is the config that will be passed to nvim_open_win.
+      --        -- Change values here to customize the layout
+      --        override = function(conf)
+      --          return conf
+      --        end,
+      --      },
+      --    },
+      --    -- Optional dependencies
+      --    --  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+      --    --  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+      --    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      --    lazy = false,
     },
-    -- Optional dependencies
-    --  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    --  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
+    --    vim.keymap.set("n", "<leader>e", function()
+    --      require("oil").oil()
+    --    end, { desc = "Open Oil in float" }),
+    --  },
   },
 }
